f35409e59a848cca6922e331bdec81957cbdad2c
naolumer <naolumer3@gmail.com>
2026-01-25 11:02:51 +0300

    environment setup

---
+++ messenger-clone/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto"
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MessengerClone",
+  description: "Messenger App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistMono.variable} ${geistSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
