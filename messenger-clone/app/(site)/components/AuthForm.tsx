2efe0bd54b72df92daf1d5ffdc7f892072d83695
naolumer <naolumer3@gmail.com>
2026-01-28 12:35:53 +0300

    implemented login functionality with credentials

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import {toast} from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -47,7 +48,20 @@ function AuthForm() {
         }
 
         if (variant === 'LOGIN') {
-             // NextAuth SignIn
+             signIn('credentials',{
+                ...data,
+                redirect:false
+             })
+             .then((callback)=>{
+                if (callback?.error) {
+                    toast.error('Invalid credentials')
+                }
+
+                if(callback?.ok && !callback?.error) {
+                    toast.success('Logged in!')
+                }
+             })
+             .finally(() => setIsLoading(false))
         }
     }
 
