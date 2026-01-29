2feb269b8f481ac00124aba2eb435f58659052d8
naolumer <naolumer3@gmail.com>
2026-01-29 21:29:15 +0300

    added mobile item and mobile footer components for smaller screens

---
+++ messenger-clone/app/components/sidebar/MobileFooter.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import MobileItem from "./MobileItem"
+import useConversation from "@/app/hooks/useConversation"
+import useRoutes from "@/app/hooks/useRoute"
+
+const MobileFooter = () => {
+    const routes = useRoutes();
+    const {isOpen} = useConversation();
+
+    if (isOpen){
+        return null;
+    }
+
+    return (
+        <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-gray-300 lg:hidden">
+           {routes.map((route)=>(
+            <MobileItem
+                key= {route.href}
+                href= {route.href}
+                active = {route.active}
+                icon = {route.icon}
+                onClick = {route.onClick}/>
+           ))}
+        </div>
+    );
+}
+
+export default MobileFooter;
--- messenger-clone/app/components/sidebar/Sidebar.tsx
+++ messenger-clone/app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 async function Sidebar ({children}:{
     children : React.ReactNode;
 }) {
     return (
         <div className="h-full">
+            <MobileFooter/>
             <DesktopSidebar/>
             <main className="lg:pl-20 h-full">
                 {children}
