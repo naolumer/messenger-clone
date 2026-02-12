88f0f519d59b07357a8e9f27da4cdcc60c2c8a7b
naolumer <naolumer3@gmail.com>
2026-02-12 21:20:33 +0300

    completed modal component

--- messenger-clone/app/components/Modal.tsx
+++ messenger-clone/app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 import { Fragment } from "react";
+import { IoClose } from "react-icons/io5";
 
 interface ModalProps  {
     isOpen?: boolean;
@@ -63,6 +64,34 @@ const Modal: React.FC<ModalProps> = ({
                                 sm:w-full
                                 sm:max-w-lg
                                 sm:p-6">
+                            <div className="
+                                absolute
+                                right-0
+                                top-0
+                                hidden
+                                pr-4
+                                pt-4
+                                sm:block
+                                z-10">
+                                <button type="button"
+                                        className="
+                                            rounded-md
+                                            bg-white
+                                            text-gray-400
+                                            hover:text-gray-500
+                                            focus: outline-none
+                                            focus:ring-2
+                                            focus: ring-sky-500
+                                            focus: ring-offset-2
+                                        "
+                                        onClick={onClose}>
+                                        
+                                        <span className="sr-only">Close</span>
+                                        <IoClose className="h-6 w-6"/>
+                                </button>
+
+                            </div>
+                            {children}
 
                         </DialogPanel>
                         
