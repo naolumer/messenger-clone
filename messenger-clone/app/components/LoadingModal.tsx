e123b57f781ea841d7e1524763a65bfbf976c081
naolumer <naolumer3@gmail.com>
2026-02-20 17:48:57 +0300

    loading spinner ui improved

--- messenger-clone/app/components/LoadingModal.tsx
+++ messenger-clone/app/components/LoadingModal.tsx
@@ -27,7 +27,7 @@ const LoadingModal = ()=>{
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0">
                     <div 
-                        className="fixed inset-0 bg-gray-100 bg-opacity-50 transition-opacity"/>
+                        className="fixed inset-0 bg-black/30 backdrop-blur-[1px] transition-opacity"/>
 
                 </TransitionChild>
                 <div className="
